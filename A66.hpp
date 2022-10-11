99e3b028f17afe119f7d2642af712a983b9447a4
kojosh <111550404+kojosh@users.noreply.github.com>
2022-10-10 22:54:35 -0700

    assignment 6-6

--- A66.hpp
+++ A66.hpp
@@ -4,4 +4,29 @@
 #include <iostream>
 using namespace std;
 
+void getInput(int& a, int& b, int& c)
+{
+    cout << "Enter three integers: " << endl;
+    cin >> a >> b >> c;
+
+}
+
 // complete the function maxmin( )
+void maxmin(int A, int B, int C, int& min, int& max)
+{
+
+    for (int i = 0; i < 3; i++) {
+        if (i == 0)
+            min = max = A;
+        min = (min > A)? A : min;
+        max = (max < A)? A : max;
+        if (i == 1)
+            min = (min > B)? B : min;
+            max = (max < B)? B : max;
+        if (i == 2)
+            min = (min > C)? C : min;
+            max = (max < C)? C : max;
+    }
+
+}
+
